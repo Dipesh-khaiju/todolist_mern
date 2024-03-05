1c8f25e03f75cbc141d0cb5e927140004653e7b7
Dipesh khaiju <111327545+Dipesh-khaiju@users.noreply.github.com>
2024-03-05 22:30:37 +0545

    connect to server

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,17 +7,17 @@ function Home(){
    
     const [todos,setTodos]=  useState([]);
     useEffect(()=>{
-        axios.get("http://localhost:3000/get")
+        axios.get("https://todolist-mern-ff0n.onrender.com/get")
         .then(result=> setTodos(result.data))
         .catch(err=>console.log(err))
     },[]);
     const handleEdit=(id)=>{
-        axios.put('http://localhost:3000/update/'+id)
+        axios.put('https://todolist-mern-ff0n.onrender.com/update/'+id)
         .then(()=>{location.reload()})
         .catch(err=>console.log(err))
     }
     const handleDelete=(id)=>{
-        axios.delete('http://localhost:3000/delete/'+id)
+        axios.delete('https://todolist-mern-ff0n.onrender.com/delete/'+id)
         .then(response=>{
             if(response.status === 204){
                 window.location.reload();
